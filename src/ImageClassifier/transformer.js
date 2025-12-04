57ca0ee31faffab9e7da3209fb2e391a27407816
pearmini <subairui@icloud.com>
2025-12-03 21:59:43 -0500

    Classify video

--- src/ImageClassifier/transformer.js
+++ src/ImageClassifier/transformer.js
@@ -1,5 +1,11 @@
 import { pipeline } from "@huggingface/transformers";
-import handleArguments from "../utils/handleArguments";
+import handleArguments, { isVideo } from "../utils/handleArguments";
+import { drawToCanvas } from "../utils/imageUtilities";
+
+function chooiseDevice() {
+  if (typeof navigator !== "undefined" && navigator.gpu) return "webgpu";
+  return "wasm";
+}
 
 /**
  * @reference https://huggingface.co/docs/transformers.js/en/api/pipelines#module_pipelines.ImageClassificationPipeline
@@ -10,10 +16,12 @@ export class ImageClassifierTransformer {
     this.needToStop = false;
     this.isClassifying = false;
     this.topK = options.topK || 3;
+    this.device = chooiseDevice();
+
     this.ready = pipeline(
       "image-classification",
       "Xenova/vit-base-patch16-224",
-      options
+      { device: this.device }
     ).then((classifier) => {
       this.classifier = classifier;
       callback?.(this);
@@ -32,26 +40,40 @@ export class ImageClassifierTransformer {
       "image",
       "No input image provided. If you want to classify a video, use classifyStart."
     );
+
+    // Transfomerjs doesn't support HTMLVideoElement directly, so convert to canvas
+    const input = isVideo(image) ? drawToCanvas(image) : image;
+
+    // Convert topK to top_k for transfomerjs and get the results
     const topK = number !== undefined ? number : this.topK;
-    const results = await this.classifier(image, { top_k: topK });
+    const results = await this.classifier(input, { top_k: topK });
+
     // Normalize the results to match the format form tensorflowjs
     const normalized = results.map((result) => ({
       label: result.label,
       confidence: result.score,
     }));
+
     callback(normalized);
     this.isClassifying = false;
     return normalized;
   }
 
-  async classifyStart(input, callback) {
+  async classifyStart(inputNumOrCallback, numOrCallback, cb) {
     if (this.isClassifying || !this.classifier) return;
+
     this.needToStop = false;
-    const next = (...args) => {
+
+    const next = () => {
       if (this.needToStop) return;
-      callback(...args);
-      this.classify(input, next);
+      this.classify(inputNumOrCallback, numOrCallback, cb).then(() => {
+        // WebGPU is very fast, so we can call the next frame immediately
+        if (this.device === "webgpu") next();
+        // Wasm is very slow, so we need to wait for 1 second before calling the next frame
+        else setTimeout(next, 1000);
+      });
     };
+
     next();
   }
 
